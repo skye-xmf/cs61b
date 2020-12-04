75e60ca93853702d03884768d6750832802e5d85
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-04 16:39:12 +0800

    week2

--- Week2/src/Intlist.java
+++ Week2/src/Intlist.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class Intlist {
+    public int first;
+    public Intlist rest;
+    int s = 1;
+
+    public Intlist(int f, Intlist r){
+        first = f;
+        rest = r;
+    }
+
+    public int size(){
+        while (rest != null){
+            s += 1;
+            rest = this.rest.rest;
+        }
+        return s;
+    }
+
+    public static void main(String[] args){
+        Intlist L =new Intlist(5, null);
+        L = new Intlist(10, L);
+        L = new Intlist(15, L);
+        L = new Intlist(20, L);
+        System.out.println(L.size());
+    }
 }
