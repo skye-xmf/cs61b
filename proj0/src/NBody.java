ef053f447a59e2e0d63aa8d8930e6f9a25bb4f32
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-05 20:41:04 +0800

    NBody

--- proj0/src/NBody.java
+++ proj0/src/NBody.java
@@ -1,5 +1,6 @@
 public class NBody {
-    public void readRadius(In in){
+    public void readRadius(String filename){
+        In in = new In(filename);
         double Radius = in.readDouble();
         System.out.println("The radius of the universe is " + Radius + " .");
     }
