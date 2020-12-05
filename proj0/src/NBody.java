53d2da4a2da93c13e006672a93f35e4536688c1e
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-05 21:00:50 +0800

    NBody

--- proj0/src/NBody.java
+++ proj0/src/NBody.java
@@ -5,4 +5,16 @@ public class NBody {
         double Radius = in.readDouble();
         return Radius;
     }
+
+    public static String[] readPlanets(String filename){
+        In in = new In(filename);
+        String[] name = new String[5];
+        int i = 0;
+        while (i < 4){
+            String planet = in.readString();
+            name[i] = planet;
+            i = i + 1;
+        }
+        return name;
+    }
 }
