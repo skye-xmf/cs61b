cbd445e16d2870bfa6bb1560bca36b72bf40fe25
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-05 20:45:41 +0800

    NBody

--- proj0/src/NBody.java
+++ proj0/src/NBody.java
@@ -1,6 +1,7 @@
 public class NBody {
     public static double readRadius(String filename){
         In in = new In(filename);
+        double number = in.readDouble();
         double Radius = in.readDouble();
         return Radius;
     }
