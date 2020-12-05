da440f51d076a633a732ed12807d070c9a4497b7
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-05 11:12:31 +0800

    wuyu

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,8 +82,6 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A == null){ return null;
-        }
         IntList p = A;
         while (A.rest != null){
             A = A.rest;
