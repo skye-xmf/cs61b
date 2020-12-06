9cb1aad0122bbb766cf2a3780997978b4cc1f61a
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-06 11:10:25 +0800

    12change

--- proj0/src/NBody.java
+++ proj0/src/NBody.java
@@ -1,48 +1,66 @@
 public class NBody {
-    public static double readRadius(String filename){
+    public static double readRadius(String filename) {
         In in = new In(filename);
         int number = in.readInt();
         double Radius = in.readDouble();
         return Radius;
     }
 
-    public static Planet[] readPlanets(String filename){
+    public static Planet[] readPlanets(String filename) {
         In in = new In(filename);
         int number = in.readInt();
         double Radius = in.readDouble();
         Planet[] planets = new Planet[number];
-        for (int i = 0; i < 5; i ++){
+        for (int i = 0; i < 5; i++) {
             planets[i] = new Planet(in.readDouble(), in.readDouble(), in.readDouble(),
                     in.readDouble(), in.readDouble(), in.readString());
         }
         return planets;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         double T = Double.parseDouble(args[0]);
         double dt = Double.parseDouble(args[1]);
         String filename = args[2];
         Double Radius = readRadius(filename);
         Planet[] planets = readPlanets(filename);
 
-        /** set up the scale of the universe. */
-        StdDraw.setScale(-Radius, Radius);
+        double t = 0;
 
-        /** clear the draw window. */
-        StdDraw.clear();
+        double[] xForces = new double[planets.length];
+        double[] yForces = new double[planets.length];
 
-        /** the location of the picture. */
-        String background = "images/starfield.jpg";
-        StdDraw.picture(0, 0, background);
+        StdDraw.enableDoubleBuffering();
 
-        /** show the picture to the screen, and wait 2000 milliseconds. */
-        StdDraw.show();
-        StdDraw.pause(2000);
+        while (t <= T) {
+            for (int i = 0; i < planets.length; i++) {
+                xForces[i] = planets[i].calcNetForceExertedByX(planets);
+                yForces[i] = planets[i].calcNetForceExertedByY(planets);
+            }
+            for (int i = 0; i < planets.length; i++) {
+                planets[i].update(dt, xForces[i], yForces[i]);
+            }
 
-        for (Planet p : planets){
-            p.draw();
-            StdDraw.show();
-            StdDraw.pause(100);
+            /** set up the scale of the universe. */
+            StdDraw.setScale(-Radius, Radius);
+
+            /** clear the draw window. */
+            StdDraw.clear();
+
+            /** the location of the picture. */
+            String background = "images/starfield.jpg";
+            StdDraw.picture(0, 0, background);
+
+            for (Planet p : planets) {
+                p.draw();
+
+                /** show the picture to the screen, and wait 2000 milliseconds. */
+                StdDraw.show();
+                StdDraw.pause(10);
+
+                t += dt;
+
+            }
         }
     }
 }
