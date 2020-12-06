7d63ce96b401a2962763ce3947e8e052d5fdaf32
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-06 15:29:34 +0800

    wuyu

--- proj0/src/Planet.java
+++ proj0/src/Planet.java
@@ -5,7 +5,7 @@ public class Planet {
     public double yyVel;
     public double mass;
     public String imgFileName;
-    private static double G = 6.67e-11;
+    static final double G = 6.67e-11;
 
     public Planet(double xP, double yP, double xV, double yV, double m, String img){
         xxPos = xP;
