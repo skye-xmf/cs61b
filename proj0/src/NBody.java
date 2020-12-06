7a7a2db1004aa62e3bcb286a3bd7634ad5f1d31c
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-06 10:13:34 +0800

    8change

--- proj0/src/NBody.java
+++ proj0/src/NBody.java
@@ -17,4 +17,32 @@ public class NBody {
         }
         return planets;
     }
+
+    public static void main(String[] args){
+        double T = Double.parseDouble(args[0]);
+        double dt = Double.parseDouble(args[1]);
+        String filename = args[2];
+        readRadius(filename);
+        readPlanets(filename);
+        drawbackground();
+    }
+    public static String image = "starfield.jpg";
+
+    public static void drawbackground(){
+        /** set up the scale of the universe. */
+        In in = new In("./src/planet.txt");
+        int number = in.readInt();
+        Double Radius = in.readDouble();
+        StdDraw.setScale(-Radius, Radius);
+
+        /** clear the draw window. */
+        StdDraw.clear();
+
+        /** the location of the picture. */
+        StdDraw.picture(0, 0, "./images/starfield.jpg");
+
+        /** show the picture to the screen, and wait 2000 milliseconds. */
+        StdDraw.show();
+        StdDraw.pause(2000);
+    }
 }
