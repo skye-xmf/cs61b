78ec1257ae921e312a12f82c4d58773d131a7a29
skye-xmf <47076790+skye-xmf@users.noreply.github.com>
2020-12-06 10:39:52 +0800

    11change

--- proj0/src/Planet.java
+++ proj0/src/Planet.java
@@ -77,6 +77,10 @@ public class Planet {
         this.xxPos = this.xxPos + dt * this.xxVel;
         this.yyPos = this.yyPos + dt * this.yyVel;
     }
+
+    public void draw(){
+        StdDraw.picture(xxPos, yyPos,"images/" + imgFileName);
+    }
 }
 
 
